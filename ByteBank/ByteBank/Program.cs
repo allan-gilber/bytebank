6c1950a6c81140acdebffe3bdd28ea708b69bd8e
allan-gilber <allangilber@yahoo.com.br>
2023-01-05 15:42:15 -0300

    feat: created add and remove account methods

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -2,16 +2,33 @@
 
 public class ByteBank
 {
+    private static List<LoginData> SavedAccounts = new List<LoginData>();
+    private const  String InvalidOptionMessage = "Invalid option. Please type a valid number from the menu below:";
 
     class LoginData
     {
-        public String login { get; set; }
-        public String password { get; set; }
+        public String _login { get; set; }
+        public String _password { get; set; }
+        public String _personName { get; set; }
+        public bool _LoginStatus { get; set; } = false;
+        public decimal _AccountBalance { get; set; } = 0.00M;
+
+        public LoginData(String login,String password, String personName)
+        {
+            _login = login;
+            _password = password;
+            _personName = personName;
+        }
+
+        public override String ToString()
+        {
+            return _login + "\n" + _password + "\n" + _personName + "\n" + _LoginStatus + "\n" + _AccountBalance;
+        } 
     }
 
     class BankMenu
     {
-        private List<LoginData> _logins = new List<LoginData>();
+        private static bool MainLoopControler = true;
 
         public static void WriteMenu()
         {
@@ -25,37 +42,77 @@ public class ByteBank
             Console.Write("Digite a opção desejada: ");
         }
 
+        public static void ProcessOption() {
+
+            while (MainLoopControler)
+            {
+                BankMenu.WriteMenu();
+                String userInput = Console.ReadLine();
+                int choosedOption;
+                if (!Int32.TryParse(userInput, out choosedOption)) { Console.WriteLine(InvalidOptionMessage); continue; }
+
+                switch (choosedOption)
+                {
+                    case 0:
+                        MainLoopControler = false;
+                        break;
+                    case 1:
+                        _CreateNewAccount();
+                        break;
+                    case 2:
+                        _DeleteAccount();
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine(InvalidOptionMessage);
+                        break;
+                }
+
+               
+            }
+        }
+
+        private static void _CreateNewAccount()
+        {
+            Console.WriteLine("Please type your login:");
+            String? login = Console.ReadLine();
+            Console.WriteLine("Please type a password for your account:");
+            String? password = Console.ReadLine();
+            Console.WriteLine("Please type your name:");
+            String? personName = Console.ReadLine();
+
+            if (login == null || password == null || personName == null) Console.WriteLine("Invalid credentials");
+            
+            SavedAccounts.Add(new LoginData(login, password, personName));
+            return;
+        }
+
+        private static void _DeleteAccount()
+        {
+            Console.WriteLine("Type the account login to be deleted:");
+            String? userInput = Console.ReadLine();
+
+            if (userInput == null) { Console.WriteLine("Invalid account"); return; }
+            int indexOfTheAccount= SavedAccounts.FindIndex(account => account._login == userInput);
+            if(indexOfTheAccount == -1) { Console.WriteLine("Account not found"); return; }
+
+            SavedAccounts.RemoveAt(indexOfTheAccount);
+            Console.WriteLine("The account was succesfully removed!");
+        }
+
+
     }
 
     public static void Main(string[] args)
     {
-        BankMenu BankMenu = new BankMenu();
-        bool mainLoopControler = true;
-
-        BankMenu.WriteMenu();
-
-        while (mainLoopControler)
-        {
-            int choosedOption = Int16.Parse(Console.ReadLine());
-
-            switch (choosedOption)
-            {
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-            }
-        }
-
+        BankMenu.ProcessOption();
+        Console.Write("The application has been successfully terminated");
     }
 }
